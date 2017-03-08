5ee0bf2aeb5b5920a323731b509f28f3e9d87fbe
leosuen <computer.king.suen@gmail.com>
2017-03-08 10:30:07 +0800

    beta 0.3

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function openTab(event,tabContent){
 	var i;
 	var x = document.getElementByClassName("resume");
+	for (var i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	document.getElementById(tabContent).style.display = "block";
 }
