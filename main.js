50da1579e378ec87bfd35c8c2b23f356bd031998
leosuen <computer.king.suen@gmail.com>
2017-03-08 10:50:35 +0800

    beta 0.3

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+document.getElementByClassName("w3-item-bar").click = function(){
+
+};
+
 function openTab(event,tabContent){
 	var i;
 	var x = document.getElementByClassName("resume");
 	for (var i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
 	}
-	document.getElementById(tabContent).style.display = "block";
+	document.getElementById(tabContent+"-resume").style.display = "block";
 }
